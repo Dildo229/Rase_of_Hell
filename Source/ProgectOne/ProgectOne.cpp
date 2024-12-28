4a7a4f4758cba89e4879ddc5fda5e0d1d37b3b45
Dildo229 <butvilovskaai@gmail.com>
2024-12-28 20:00:46 +0300

    Initial commit

---
+++ Source/ProgectOne/ProgectOne.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProgectOne.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProgectOne, "ProgectOne" );
+ 
